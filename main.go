78f578203f4c3e85433bae5709b1d9fb2679f7af
Robert Podschwadt <robertpodschwadt@gmail.com>
2018-04-18 02:47:02 -0500

    implemented logical clocks

--- main.go
+++ main.go
@@ -14,7 +14,19 @@ import(
 const N = 5
 const startPort = 14000
 
-const request = "request"
+// all messages start with a timestamp as the first "argument"
+// a message is a list of strings seperated by a ;
+// general format: timestamp;<command>;[parameters]\n
+// they must be terminated by a \n
+
+
+const REQUEST = "request" // id
+const INQUIRE = "inquire" // id
+const RELINQUISH = "relinquish" // id
+const LOCKED = "locked" // id
+const RELEASE = "rlease" // id
+const FAILED = "failed"
+
 const grant = "grant"
 const failed = "failed"
 const release = "rlease"
@@ -40,7 +52,7 @@ func create( id int, done chan bool ){
     // var grantQueue []int
     var requestQueue []int
     var mutex = &sync.Mutex{}
-
+    logicalTime := 0 // logical timestamp
 
     //server
     go func(){
@@ -49,27 +61,40 @@ func create( id int, done chan bool ){
         if err != nil {
           fmt.Print( "accept failed" )
         }
-        go func(){
-          conn := conn
+        go func( conn net.Conn ){
           msg, err := bufio.NewReader(conn).ReadString('\n')
           if err != nil {
             fmt.Print( "to dubm to read" )
           }
-          fmt.Printf( "Recieved message at node: %d \n \tmessage: %s", id, msg )
 
+          //split message recieved
           splits := strings.Split( msg, ";" )
 
-          switch splits[ 0 ] {
-          case request:
-              // id, timestamp
+          // update logical clock
+          senderTimestamp, _ := strconv.Atoi( splits[ 0 ] )
+          mutex.Lock()
+          if logicalTime + 1 < senderTimestamp {
+            logicalTime = senderTimestamp
+          } else{
+            logicalTime ++
+          }
+          mutex.Unlock()
+
+          fmt.Printf( "Recieved message at node: %d \n \tmessage: %s", id, msg )
+
+          // interpret message
+          cmd := splits[ 1 ]
+          switch cmd {
+          case REQUEST:
+              // id
               senderId, _ := strconv.Atoi( splits[ 1 ] )
               push( &requestQueue, senderId, mutex )
               //strconv.ParseInt( splits[ 2 ] )
           default:
-            panic( "protocol violation" )
+            panic( fmt.Sprintf( "protocol violation: %s", cmd ) )
           }
 
-        }()
+        }( conn )
       }
     }()
 
@@ -85,7 +110,12 @@ func create( id int, done chan bool ){
         if err != nil {
           fmt.Print( "sent failed" )
         }
-        fmt.Fprintf( conn, "request;%d", id  )
+
+        //update logical clock
+        mutex.Lock()
+        logicalTime ++
+        mutex.Unlock()
+        fmt.Fprintf( conn, "%d;%s;%d\n", logicalTime, REQUEST, id  )
       }
     }()
 
